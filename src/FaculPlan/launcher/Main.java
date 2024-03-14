85a3df9ce76773d634ac2c11a5f09bf859cc2d91
atbcj <atbcj2004@gmail.com>
2024-03-14 17:05:14 +0100

    Update

--- src/FaculPlan/launcher/Main.java
+++ src/FaculPlan/launcher/Main.java
@@ -1,11 +1,18 @@
 package FaculPlan.launcher;
 
+import java.text.DecimalFormat;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
-		int x = 0;
-
+		DecimalFormat df = new DecimalFormat("#.00");
+		double x = 1.4159999999;
+		System.out.println(df.format(x));
+		String st = df.format(x);
+		double y = Double.parseDouble(st);
+		System.out.println(y);
+		System.out.println(df.format(x));
 	}
 
 }
