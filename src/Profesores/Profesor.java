8a4374252836e8dcd438f04dddf86b72acfa89ec
Alee200210 <alisicka@ucm.es>
2024-04-18 18:28:52 +0200

    -

--- src/Profesores/Profesor.java
+++ src/Profesores/Profesor.java
@@ -2,4 +2,17 @@ package Profesores;
 
 public class Profesor {
 
+	// atributos
+	private String _nombre;
+	private String[] _apellidos;
+	private String _DNI;
+	private String _correo;
+
+	public Profesor(String _nombre, String[] _apellidos, String DNI, String correo) {
+		
+		this._nombre = _nombre;
+		this._apellidos = _apellidos;
+		this._DNI = DNI;
+		this._correo = correo;
+	}
 }
