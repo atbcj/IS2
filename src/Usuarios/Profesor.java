0e3cf4887cdd8ab9de4a326ef9a8ff94397816e6
Alee200210 <alisicka@ucm.es>
2024-05-09 13:42:23 +0200

    0

--- src/Usuarios/Profesor.java
+++ src/Usuarios/Profesor.java
@@ -2,13 +2,9 @@ package Usuarios;
 
 public class Profesor extends Usuario{
 
-	// atributos
-	
-
 	public Profesor(String nombre, String apellidos, String DNI, String correo) {
+		
 		super(nombre,apellidos,DNI,correo);
 	}
-	
-	// metodos
 
 }
